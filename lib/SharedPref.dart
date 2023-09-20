6ebedf9a897df321ad8fd8ff769c5f2923315b56
Jamil <jhossain@powerley.com>
2023-09-20 11:35:59 +0600

    appodeal logic implement

---
+++ lib/SharedPref.dart
@@ -0,0 +1,36 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPref {
+  //save string
+  static save(String key, String message) async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setString(key, message);
+  }
+
+  static read(String key) async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    return pref.getString(key);
+  }
+
+  //save int
+  static setInt(String key, int value) async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setInt(key, value);
+  }
+
+  static getInt(String key) async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    return pref.getInt(key);
+  }
+
+  //save Bool
+  static setBool(String key, bool value) async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setBool(key, value);
+  }
+
+  static getBool(String key) async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    return pref.getBool(key);
+  }
+}
--- lib/sura_detail.dart
+++ lib/sura_detail.dart
@@ -1,3 +1,4 @@
+import 'package:al_quran/SharedPref.dart';
 import 'package:al_quran/admob.dart';
 import 'package:al_quran/interstitial_admob.dart';
 import 'package:flutter/material.dart';
@@ -19,18 +20,15 @@ class SuraDetailScreen extends StatefulWidget {
   State<SuraDetailScreen> createState() => _SuraDetailScreenState();
 }
 
-class _SuraDetailScreenState extends State<SuraDetailScreen> {
-
-  Future<String> getFromLocalMemory(String key) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String user = prefs.getString(key) as String;
-    return user;
-  }
+class _SuraDetailScreenState extends State<SuraDetailScreen>{
 
   //admob
   InterstitialAd? _interstitialAd;
-  final _gameLength = 30;
-  late var _counter = _gameLength;
+  ///var _delayMax = 30;
+  var delayMin = 12;
+  //final int _gameLength = SharedPref.getInt('K_AD_TIME_INTERVAL') ?? 30;
+  final int _delayMax = 12;
+  late var _counter = _delayMax;
 
   final String _adUnitId = Platform.isAndroid
       ? 'ca-app-pub-9133033983333483/4055714014'
@@ -45,19 +43,11 @@ class _SuraDetailScreenState extends State<SuraDetailScreen> {
     super.initState();
     _startLoadNewAd();
 
-    // var userInfo;
-    // getFromLocalMemory('user').then((value) =>
-    // userInfo = value
-    // );
-    //
-    // print('userInfouserInfo::$userInfo');
-
-    // Set ad auto caching enabled or disabled
-// By default autocache is enabled for all ad types
+    // By default autocache is enabled for all ad types
     Appodeal.setAutoCache(AppodealAdType.Interstitial, true); //default - true
     //Appodeal.cache(AppodealAdType.Interstitial);
 
-// Set testing mode
+    // Set testing mode
     Appodeal.setTesting(true); //default - false
 
     Appodeal.initialize(
@@ -78,7 +68,8 @@ class _SuraDetailScreenState extends State<SuraDetailScreen> {
 
   void _startLoadNewAd() {
     sleep(const Duration(seconds: 1));
-    setState(() => _counter = _gameLength);
+    setState(() => _counter = _delayMax);
+    //_counter = _gameLength;
     _loadAd();
     _starTimer();
   }
@@ -155,8 +146,9 @@ class _SuraDetailScreenState extends State<SuraDetailScreen> {
   }
 
   void _starTimer() {
-    Timer.periodic(const Duration(seconds: 20), (timer) async {
+    Timer.periodic(const Duration(seconds: 5), (timer) async {
       setState(() => _counter--);
+      //_counter-- ;
       if (_counter == 1){
         _loadAd();
 
@@ -171,7 +163,7 @@ class _SuraDetailScreenState extends State<SuraDetailScreen> {
             onInterstitialExpired: () => {});
       }
 
-      if (!isLoadFirstAd && _counter == _gameLength - 1){
+      if (!isLoadFirstAd && _counter == _delayMax - 1){
         _loadAd();
         isLoadFirstAd = true;
       }
@@ -184,6 +176,7 @@ class _SuraDetailScreenState extends State<SuraDetailScreen> {
         // Check that interstitial
         var isCanShow = await Appodeal.canShow(AppodealAdType.Interstitial);
         if (isCanShow) {
+          saveTime();
           Appodeal.show(AppodealAdType.Interstitial);
         }
 
@@ -193,9 +186,128 @@ class _SuraDetailScreenState extends State<SuraDetailScreen> {
     });
   }
 
+  void saveTime() async {
+    print('====== save time method ======');
+
+    //Obtain shared preferences.
+    final prefs = await SharedPreferences.getInstance();
+    var today = prefs.getString('K_TODAY');
+    var yesterday = prefs.getString('K_YESTERDAY');
+
+    print('====== K_TODAY::$today');
+    print('====== K_YESTERDAY::$yesterday');
+
+    if(today == null){
+      await prefs.setString('K_TODAY', DateTime.now().toString());
+      today = prefs.getString('K_TODAY');
+      SharedPref.setInt('K_AD_TIME_INTERVAL', _delayMax);
+    }
+
+    DateTime todayDate = DateTime.parse(today!);
+    var todayHour = DateFormat.H().format(todayDate);
+    var nowHour = DateFormat.H().format(DateTime.now());
+
+    if (yesterday == null){
+      var timestamp = DateTime.now().millisecondsSinceEpoch - 1*3600*1000; //3 hours past in mili sec
+      final DateTime yesterdayDate = DateTime.fromMillisecondsSinceEpoch(timestamp);
+      await prefs.setString('K_YESTERDAY', yesterdayDate.toString());
+      yesterday = prefs.getString('K_YESTERDAY');
+    }
+
+    var yesterdayDate = DateTime.parse(yesterday!);
+    var yesterdayHour = DateFormat.H().format(yesterdayDate);
+
+    print('todayHour:$todayHour');
+    print('yesterdayHour:$yesterdayHour');
+    print('nowHour:$nowHour');
+
+    //save todays ads
+    var todaysAds = prefs.getInt('K_TODAYS_ADS');
+    var yesterdaysAds = prefs.getInt('K_YESTERDAYS_ADS');
+
+    if(nowHour == todayHour){
+      var totalTodaysAds = 0;
+      if (todaysAds == null){
+        totalTodaysAds = 1;
+      }else{
+        totalTodaysAds = todaysAds + 1;
+      }
+      await prefs.setInt('K_TODAYS_ADS', totalTodaysAds);
+    }else{
+      //save yesterdays ads
+      // yesterdayHour = todayHour;
+      // todayHour = nowHour;
+
+      await prefs.setString('K_TODAY', today);
+      await prefs.setString('K_YESTERDAY', yesterday);
+
+      //todays ads passing into yesterday
+      var totalYesterdaysAds = prefs.getInt('K_TODAYS_ADS') ?? 0;
+      await prefs.setInt('K_YESTERDAYS_ADS', totalYesterdaysAds);
+      await prefs.setInt('K_TODAYS_ADS', 1);
+
+      //check if previous day is yesterday
+      if (todayDate != null) {
+        DateTime tempYesterday = todayDate.subtract(Duration(hours: 1));
+        var tempYesterdayHour = DateFormat.H().format(tempYesterday);
+            if (tempYesterdayHour == yesterdayHour){ //real yesterday
+              await prefs.setInt('K_YESTERDAYS_ADS', totalYesterdaysAds);
+            }else{ // long before yesterday
+              await prefs.setInt('K_YESTERDAYS_ADS', 0);
+            }
+      }
+
+      //calculate new ad interval
+      if(totalYesterdaysAds >0){
+        var decreaseTodaysInterval = prefs.getInt('K_AD_TIME_INTERVAL') ?? 0;
+        if(decreaseTodaysInterval > _delayMax){
+          decreaseTodaysInterval = _delayMax; //set max to 30*20 = 600 sec, 10 min
+          await prefs.setInt('K_AD_TIME_INTERVAL', decreaseTodaysInterval);
+        }else{
+          if(decreaseTodaysInterval <= 3){
+            decreaseTodaysInterval = 3; //set min to 3*20 = 60 sec, 1 min
+            //3*(20 sec interval) = 60 seconds, so increase 1 min
+            await prefs.setInt('K_AD_TIME_INTERVAL', decreaseTodaysInterval);
+          }else{
+            decreaseTodaysInterval =  decreaseTodaysInterval - 3; //3*(20 sec interval) = 60 seconds, so decrease 1 min
+            await prefs.setInt('K_AD_TIME_INTERVAL', decreaseTodaysInterval);
+          }
+        }
+      }else{ //yesterday did not used
+        var increaseTodaysInterval = prefs.getInt('K_AD_TIME_INTERVAL') ?? 0;
+        if(increaseTodaysInterval >= _delayMax){
+          increaseTodaysInterval = _delayMax; //set max to 30*20 = 600 sec, 10 min
+          await prefs.setInt('K_AD_TIME_INTERVAL', increaseTodaysInterval);
+        }else{
+          if(increaseTodaysInterval < 3){
+            increaseTodaysInterval = 3; //set min to 3*20 = 60 sec, 1 min
+            //3*(20 sec interval) = 60 seconds, so increase 1 min
+            await prefs.setInt('K_AD_TIME_INTERVAL', increaseTodaysInterval);
+          }else{
+            increaseTodaysInterval = increaseTodaysInterval + 3; //3*(20 sec interval) = 60 seconds, so increase 1 min
+            await prefs.setInt('K_AD_TIME_INTERVAL', increaseTodaysInterval);
+          }
+        }
+      } //end interval
+    }
+
+    print('today:$today');
+    print('yesterday:$yesterday');
+
+    todaysAds = await SharedPref.getInt('K_TODAYS_ADS');
+    yesterdaysAds = await SharedPref.getInt('K_YESTERDAYS_ADS');
+    var adInterval = await SharedPref.getInt('K_AD_TIME_INTERVAL');
+
+    print('todaysAds:$todaysAds');
+    print('yesterdaysAds:$yesterdaysAds');
+    print('K_AD_TIME_INTERVAL:$adInterval');
+  }
+
   // @override
   // void dispose() {
   //   _interstitialAd?.dispose();
   //   super.dispose();
   // }
 }
+
+
